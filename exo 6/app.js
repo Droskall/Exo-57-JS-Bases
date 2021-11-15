bc34b7c436dbeaa18b3be68b99701dc4c43ea8b9
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-11-15 14:08:40 +0100

    finish

---
+++ exo 6/app.js
@@ -0,0 +1,7 @@
+let age = +prompt("quel age a tu ?");
+
+if(age === parseInt(age)) {
+    if (age > 18) alert("Vous êtes majeur ");
+    else alert("Vous etes mineur")
+}
+else alert("entrez un entier SVP")
